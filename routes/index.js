9c3d66d57a04030a4ff0c0148d6a35444dee94db
Rikesh Makwana <rikesh.makwana@gmail.com>
2018-12-24 13:30:38 +0400

    Restructure Files
    Restructure the files to make it easy to read and added gitignore file to prevent upload of node modules.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Heriot Watt - Tech Club' });
+});
+
+module.exports = router;
